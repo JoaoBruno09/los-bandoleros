3e10b47b8d1056b6049b8bb27b21aa54c74366ad
Ricardo Dias <1210126@isep.ipp.pt>
2022-11-28 16:29:52 +0000

    Revert "jwt authorization created"
    This reverts commit 2f1d9ccd2d4b733ef7314726c450bbfed7e58ca7.

--- app.ts
+++ app.ts
@@ -1,13 +1,11 @@
 import express, { Express, Request, Response } from "express";
-import { router as routerPlan } from "./src/routes/Plans";
-import { router as routerAuth } from "./src/middlewares/Auth";
+import { router as routerPlans } from "./src/routes/Plans";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-//APP ROUTES
-app.use("/auth", routerAuth);
-app.use("/plan", routerPlan);
+//ROUTES
+app.use("/plans", routerPlans);
 
 app.use("/", (req: Request, res: Response) => {
   res.status(404).send("404 NOT FOUND");
--- src/routes/Plans.ts
+++ src/routes/Plans.ts
@@ -1,17 +1,17 @@
 import { Router } from "express";
-import { authenticateJWT } from "../middlewares/Auth";
 
 export const router = Router();
 
-//PLANS ROUTES
 router.get("/", (req, res) => {
   res.status(200).json({
     message: "GET ALL PLANS",
   });
 });
 
-router.post("/", authenticateJWT, (req, res) => {
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
   res.status(200).json({
     message: "POST - CREATE NEW PLAN",
+    id: id,
   });
 });
