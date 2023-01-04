14d9a0e3d0bc154be673e62b925214c3667e4abb
João Rocha <1220256@isep.ipp.pt>
2023-01-04 19:53:37 +0000

    Added 1.1, 1.2 and 1.4 ruse cases to the project, close #2, close #3, close #5

--- services/plans/src/models/plansModel.ts
+++ services/plans/src/models/plansModel.ts
@@ -8,23 +8,23 @@ export enum musicSuggestionsEnum {
 export interface Plan {
   PID: string;
   name: string;
-  description: string;
-  numberOfMinutes: String;
-  maximumNumberOfUsersDevices: number;
-  musicCollections: number;
+  description?: string;
+  numberOfMinutes?: String;
+  maximumNumberOfDevices: number;
+  musicCollections?: number;
   musicSuggestions: musicSuggestionsEnum;
   monthlyFee: number;
   anualFee: number;
-  isPromoted: boolean;
-  isActive: boolean;
+  isPromoted?: boolean;
+  isActive?: boolean;
 }
 
 const planSchema = new mongoose.Schema<Plan>({
   PID: { type: String, required: true },
   name: { type: String, required: true },
-  description: { type: String, required: false },
+  description: { type: String, required: false, default: "" },
   numberOfMinutes: { type: String, required: false, default: "Unlimited" },
-  maximumNumberOfUsersDevices: { type: Number, required: true },
+  maximumNumberOfDevices: { type: Number, required: true },
   musicCollections: { type: Number, required: false, default: 0 },
   musicSuggestions: { type: String, required: true },
   monthlyFee: { type: Number, required: true },
--- services/plans/src/routes/plansRouter.ts
+++ services/plans/src/routes/plansRouter.ts
@@ -1,56 +1,79 @@
 import { Router } from "express";
 import { authenticateJWT } from "../middlewares/Auth";
 import { planModel, Plan, musicSuggestionsEnum } from "../models/plansModel";
+import crypto from "crypto";
 import plans from "../config/plans.json";
 import { db } from "../config/DatabaseConfig";
+import * as jwt from "jsonwebtoken";
 
+const accessTokenSecret: string = "the-secret-key";
 export const planRouter = Router();
 
 //PLANS ROUTES
 
 //POST - CREATE NEW PLAN 1.1
-planRouter.post("/", authenticateJWT, (req, res) => {
-  // GENERATE PID
-  /*
-  crypto.generateKey("hmac", { length: 40 }, (err, key) => {
-    if (err) throw err;
-    console.log("P" + key.export().toString("hex"));
-  });
-  */
-  plans.push(req.body);
-  res.status(200).json({
-    message: "POST - CREATE NEW PLAN",
-    plans: plans,
-  });
-});
+planRouter.post("/", (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, accessTokenSecret, (err: any, user: any) => {
+      if (user.role !== "Marketing Director") {
+        res
+          .status(401)
+          .send("Unauthorized. You don't have access to this resource!");
+      } else {
+        crypto.generateKey("hmac", { length: 40 }, (err, key) => {
+          if (err) throw err;
+          const PID = "P" + key.export().toString("hex");
+          const newPlan: Plan = {
+            PID: PID,
+            name: req.body.name,
+            description: req.body.description,
+            maximumNumberOfDevices: req.body.maximumNumberOfDevices,
+            numberOfMinutes: req.body.numberOfMinutes,
+            musicCollections: req.body.musicCollections,
+            musicSuggestions: req.body.musicSuggestions,
+            monthlyFee: req.body.monthlyFee,
+            anualFee: req.body.anualFee,
+          };
 
-//DELETE - DEACTIVATE PLAN 1.2
-planRouter.delete("/:id", (req, res) => {
-  const planIndex = plans.findIndex((plan) => plan.PID === req.params.id);
-  if (planIndex >= 0) plans[planIndex].isActive = false;
-  res.status(200).json({
-    message: `DELETE - DEACTIVATE PLAN ${req.params.id}`,
-    plans: plans,
-  });
-});
-
-//PUT - UPDATE PLAN 1.3
-planRouter.put("/:id", authenticateJWT, (req, res) => {
-  const planIndex = plans.findIndex((plan) => plan.PID === req.params.id);
-  if (planIndex >= 0) plans[planIndex].name = req.body.plan;
-  res.status(200).json({
-    message: `PUT - UPDATE PLAN ${req.params.id}`,
-    plans: plans,
-  });
+          if (
+            newPlan.musicSuggestions === musicSuggestionsEnum.automatic ||
+            newPlan.musicSuggestions === musicSuggestionsEnum.personalized
+          ) {
+            if (db) {
+              planModel.create(newPlan).then(() => {
+                planModel
+                  .find({ PID: newPlan.PID }, { _id: 0, __v: 0 })
+                  .then((plan) => {
+                    res.status(201).json(plan);
+                  });
+              });
+            } else {
+              res.status(500).send("Internal Server Error");
+            }
+          } else {
+            res.status(400).send("Bad Request");
+          }
+        });
+      }
+    });
+  } else {
+    res.status(401).send("Unauthorized. Access token is missing!");
+  }
 });
 
 //GET - GET ALL PLANS 1.4
 planRouter.get("/", (req, res) => {
   if (db) {
-    planModel.find({}, { _id: 0, isActive: 0 }).then((plan) => {
-      res.status(200).json({
-        plans: plan,
-      });
+    planModel.find({ isActive: true }, { _id: 0, __v: 0 }).then((plan) => {
+      if (plan.length > 0) {
+        res.status(200).json({
+          plans: plan,
+        });
+      } else {
+        res.status(404).json("Not found. Plans not found or don't exist!");
+      }
     });
   } else {
     res.status(500).send();
@@ -61,13 +84,64 @@ planRouter.get("/", (req, res) => {
 planRouter.get("/:PID", (req, res) => {
   if (db) {
     planModel
-      .find({ PID: req.params.PID }, { _id: 0, isActive: 0 })
+      .find({ PID: req.params.PID, isActive: true }, { _id: 0, __v: 0 })
       .then((plan) => {
-        res.status(200).json(plan);
+        if (plan.length > 0) {
+          res.status(200).json(plan);
+        } else {
+          res.status(404).json("Not found. Plan not found or doesn't exist!");
+        }
       });
   } else {
     res.status(500).send();
   }
+});
+
+//PUT - DEACTIVATE PLAN 1.2
+planRouter.put("/:PID", (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, accessTokenSecret, (err: any, user: any) => {
+      if (user.role !== "Marketing Director") {
+        res
+          .status(401)
+          .send("Unauthorized. You don't have access to this resource!");
+      } else {
+        if (db) {
+          planModel
+            .findOneAndUpdate({ PID: req.params.PID }, { isActive: false })
+            .then((response) => {
+              if (!response?.$isEmpty) {
+                planModel
+                  .find({ PID: req.params.PID }, { _id: 0, __v: 0 })
+                  .then((plan) => {
+                    res.status(200).json(plan);
+                  });
+              } else {
+                res
+                  .status(404)
+                  .json("Not found. Plan not found or doesn't exist!");
+              }
+            });
+        } else {
+          res.status(500).send();
+        }
+      }
+    });
+  } else {
+    res.status(401).send("Unauthorized. Access token is missing!");
+  }
+});
+
+//PUT - UPDATE PLAN 1.3
+planRouter.put("/:id", authenticateJWT, (req, res) => {
+  const planIndex = plans.findIndex((plan) => plan.PID === req.params.id);
+  if (planIndex >= 0) plans[planIndex].name = req.body.plan;
+  res.status(200).json({
+    message: `PUT - UPDATE PLAN ${req.params.id}`,
+    plans: plans,
+  });
 });
 
 //GET - GET PLAN
