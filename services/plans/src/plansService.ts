1604045541db951d2c664e984d4569818ba2df73
João Rocha <1220256@isep.ipp.pt>
2023-01-04 23:37:59 +0000

    Added more resources to plans, close #4

--- services/plans/src/plansService.ts
+++ services/plans/src/plansService.ts
@@ -1,8 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import { load } from "ts-dotenv";
-//import { db } from "./src/config/DatabaseConfig";
 import { planRouter } from "./routes/plansRouter";
-import { authRouter } from "./middlewares/Auth";
 
 const env = load({
   PORT: Number,
@@ -11,15 +9,10 @@ const env = load({
 const app: Express = express();
 const port = env.PORT || 3001;
 app.use(express.json());
-app.use("/plan", planRouter, (req, res) => {
-  console.log(req.body);
-});
+app.use("/plan", planRouter);
 
-//app.use("/auth", authRouter);
 app.listen(port, () => {
   console.log(
     `⚡️[server]: Plans Service is running at http://localhost:${port}`
   );
-  //db.on("error", (error) => console.log(error));
-  //db.once("open", () => console.log("Conected to Database"));
 });
--- services/plans/src/routes/plansRouter.ts
+++ services/plans/src/routes/plansRouter.ts
@@ -1,8 +1,6 @@
 import { Router } from "express";
-import { authenticateJWT } from "../middlewares/Auth";
 import { planModel, Plan, musicSuggestionsEnum } from "../models/plansModel";
 import crypto from "crypto";
-import plans from "../config/plans.json";
 import { db } from "../config/DatabaseConfig";
 import * as jwt from "jsonwebtoken";
 
@@ -42,13 +40,18 @@ planRouter.post("/", (req, res) => {
             newPlan.musicSuggestions === musicSuggestionsEnum.personalized
           ) {
             if (db) {
-              planModel.create(newPlan).then(() => {
-                planModel
-                  .find({ PID: newPlan.PID }, { _id: 0, __v: 0 })
-                  .then((plan) => {
-                    res.status(201).json(plan);
-                  });
-              });
+              planModel.create(newPlan).then(
+                () => {
+                  planModel
+                    .find({ PID: newPlan.PID }, { _id: 0, __v: 0 })
+                    .then((plan) => {
+                      res.status(201).json(plan);
+                    });
+                },
+                (error) => {
+                  res.status(400).send("Bad Request");
+                }
+              );
             } else {
               res.status(500).send("Internal Server Error");
             }
@@ -66,15 +69,20 @@ planRouter.post("/", (req, res) => {
 //GET - GET ALL PLANS 1.4
 planRouter.get("/", (req, res) => {
   if (db) {
-    planModel.find({ isActive: true }, { _id: 0, __v: 0 }).then((plan) => {
-      if (plan.length > 0) {
-        res.status(200).json({
-          plans: plan,
-        });
-      } else {
-        res.status(404).json("Not found. Plans not found or don't exist!");
+    planModel.find({ isActive: true }, { _id: 0, __v: 0 }).then(
+      (plan) => {
+        if (plan.length > 0) {
+          res.status(200).json({
+            plans: plan,
+          });
+        } else {
+          res.status(404).json("Not found. Plans not found or don't exist!");
+        }
+      },
+      (error) => {
+        res.status(500).send("Internal Server Error");
       }
-    });
+    );
   } else {
     res.status(500).send();
   }
@@ -85,15 +93,20 @@ planRouter.get("/:PID", (req, res) => {
   if (db) {
     planModel
       .find({ PID: req.params.PID, isActive: true }, { _id: 0, __v: 0 })
-      .then((plan) => {
-        if (plan.length > 0) {
-          res.status(200).json(plan);
-        } else {
-          res.status(404).json("Not found. Plan not found or doesn't exist!");
+      .then(
+        (plan) => {
+          if (plan.length > 0) {
+            res.status(200).json(plan);
+          } else {
+            res.status(404).json("Not found. Plan not found or doesn't exist!");
+          }
+        },
+        (error) => {
+          res.status(500).send("Internal Server Error");
         }
-      });
+      );
   } else {
-    res.status(500).send();
+    res.status(500).send("Internal Server Error");
   }
 });
 
@@ -111,21 +124,26 @@ planRouter.put("/:PID", (req, res) => {
         if (db) {
           planModel
             .findOneAndUpdate({ PID: req.params.PID }, { isActive: false })
-            .then((response) => {
-              if (!response?.$isEmpty) {
-                planModel
-                  .find({ PID: req.params.PID }, { _id: 0, __v: 0 })
-                  .then((plan) => {
-                    res.status(200).json(plan);
-                  });
-              } else {
-                res
-                  .status(404)
-                  .json("Not found. Plan not found or doesn't exist!");
+            .then(
+              (response) => {
+                if (response !== null) {
+                  planModel
+                    .find({ PID: req.params.PID }, { _id: 0, __v: 0 })
+                    .then((plan) => {
+                      res.status(201).json(plan);
+                    });
+                } else {
+                  res
+                    .status(404)
+                    .json("Not found. Plan not found or doesn't exist!");
+                }
+              },
+              (error) => {
+                res.status(400).send("Bad Request");
               }
-            });
+            );
         } else {
-          res.status(500).send();
+          res.status(500).send("Internal Server Error");
         }
       }
     });
@@ -135,16 +153,188 @@ planRouter.put("/:PID", (req, res) => {
 });
 
 //POST - UPDATE DETAILS OF PLAN 1.3
-planRouter.post("/:PID", (req, res) => {});
+planRouter.post("/:PID", (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, accessTokenSecret, (err: any, user: any) => {
+      if (user.role !== "Marketing Director") {
+        res
+          .status(401)
+          .send("Unauthorized. You don't have access to this resource!");
+      } else {
+        const updatePlan = {
+          name: req.body?.name,
+          description: req.body?.description,
+          maximumNumberOfDevices: req.body?.maximumNumberOfDevices,
+          numberOfMinutes: req.body?.numberOfMinutes,
+          musicCollections: req.body?.musicCollections,
+          musicSuggestions: req.body?.musicSuggestions,
+        };
+
+        if (
+          updatePlan?.musicSuggestions === undefined ||
+          updatePlan?.musicSuggestions === musicSuggestionsEnum.automatic ||
+          updatePlan?.musicSuggestions === musicSuggestionsEnum.personalized
+        ) {
+          if (db) {
+            planModel
+              .findOneAndUpdate({ PID: req.params.PID }, updatePlan)
+              .then(
+                (response) => {
+                  if (response !== null) {
+                    planModel
+                      .find({ PID: req.params.PID }, { _id: 0, __v: 0 })
+                      .then((plan) => {
+                        res.status(200).json(plan);
+                      });
+                  } else {
+                    res
+                      .status(404)
+                      .json("Not found. Plan not found or doesn't exist!");
+                  }
+                },
+                (error) => {
+                  res.status(400).send("Bad Request");
+                }
+              );
+          } else {
+            res.status(500).send("Internal Server Error");
+          }
+        } else {
+          res.status(400).send("Bad Request");
+        }
+      }
+    });
+  } else {
+    res.status(401).send("Unauthorized. Access token is missing!");
+  }
+});
 
 //DELETE - DELETE PLAN 9.2
-planRouter.delete("/:PID", (req, res) => {});
+planRouter.delete("/:PID", (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, accessTokenSecret, (err: any, user: any) => {
+      if (user.role !== "Marketing Director") {
+        res
+          .status(401)
+          .send("Unauthorized. You don't have access to this resource!");
+      } else {
+        if (db) {
+          planModel.findOneAndDelete({ PID: req.params.PID }).then(
+            (response) => {
+              if (response !== null) {
+                res.status(2010);
+              } else {
+                res
+                  .status(404)
+                  .json("Not found. Plan not found or doesn't exist!");
+              }
+            },
+            (error) => {
+              res.status(500).send("Internal Server Error");
+            }
+          );
+        } else {
+          res.status(500).send("Internal Server Error");
+        }
+      }
+    });
+  } else {
+    res.status(401).send("Unauthorized. Access token is missing!");
+  }
+});
 
 //PATCH - CHANGE PRICE OF PLAN 9.3
-planRouter.patch("/:PID", (req, res) => {});
+planRouter.patch("/:PID", (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, accessTokenSecret, (err: any, user: any) => {
+      if (user.role !== "Marketing Director") {
+        res
+          .status(401)
+          .send("Unauthorized. You don't have access to this resource!");
+      } else {
+        const monthlyFeeIncom: number = req.body.monthlyFee;
+        const anualFeeIncom: number = req.body.anualFee;
+        if (db) {
+          planModel
+            .findOneAndUpdate(
+              { PID: req.params.PID },
+              { monthlyFee: monthlyFeeIncom, anualFee: anualFeeIncom }
+            )
+            .then(
+              (response) => {
+                if (response !== null) {
+                  planModel
+                    .find({ PID: req.params.PID }, { _id: 0, __v: 0 })
+                    .then((plan) => {
+                      res.status(200).json(plan);
+                    });
+                } else {
+                  res
+                    .status(404)
+                    .json("Not found. Plan not found or doesn't exist!");
+                }
+              },
+              (error) => {
+                res.status(400).send("Bad Request");
+              }
+            );
+        } else {
+          res.status(500).send("Internal Server Error");
+        }
+      }
+    });
+  } else {
+    res.status(401).send("Unauthorized. Access token is missing!");
+  }
+});
 
 //PATCH - PROMOTE PLAN 9.1
-planRouter.patch("/:PID/promotion", (req, res) => {});
+planRouter.patch("/:PID/promotion", (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, accessTokenSecret, (err: any, user: any) => {
+      if (user.role !== "Marketing Director") {
+        res
+          .status(401)
+          .send("Unauthorized. You don't have access to this resource!");
+      } else {
+        if (db) {
+          planModel
+            .findOneAndUpdate({ PID: req.params.PID }, { isPromoted: true })
+            .then(
+              (response) => {
+                if (response !== null) {
+                  planModel
+                    .find({ PID: req.params.PID }, { _id: 0, __v: 0 })
+                    .then((plan) => {
+                      res.status(200).json(plan);
+                    });
+                } else {
+                  res
+                    .status(404)
+                    .json("Not found. Plan not found or doesn't exist!");
+                }
+              },
+              (error) => {
+                res.status(500).send("Internal Server Error");
+              }
+            );
+        } else {
+          res.status(500).send("Internal Server Error");
+        }
+      }
+    });
+  } else {
+    res.status(401).send("Unauthorized. Access token is missing!");
+  }
+});
 
 //GET - PRICE CHANGE HISTORY OF A PLAN 13.2
 planRouter.get("/:PID/history", (req, res) => {});
